95aa0f173f9fdbf352ea340cc24c72323afceed9
sslava123 <sslavinskiy@me.com>
2026-04-03 11:38:09 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='z7ii-v1';
+var CACHE='z7ii-v2';
 var FILES=['./','./index.html'];
 self.addEventListener('install',function(e){
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}));
